f5fe4400282159fc5363621e6dadf2b1f5ad9bb5
danbrick92 <danbrickner@hotmail.com>
2026-04-25 17:16:34 -0400

    Update arch

---
+++ src/data_pipeline/airflow-commands.sh
@@ -0,0 +1,6 @@
+# The airflow standalone command initializes the database, creates a user, and starts all components
+airflow standalone
+
+# Visit localhost:8080 in your browser and log in with the admin account details shown in the terminal. Enable the example_bash_operator Dag in the home page.
+
+# https://airflow.apache.org/docs/apache-airflow/stable/start.html
